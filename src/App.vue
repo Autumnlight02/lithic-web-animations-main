36ef01ddc37dab74d8d3990a810cd99606f5288b
Aykut <aykut.akguen@it-suisse.com>
2023-01-12 16:57:00 +0100

    eee

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ChipAnimation from "./components/ChipAnimation/index.vue";
+import "./assets/global.css";
+</script>
+
+<template>
+  <div class="container">
+    <ChipAnimation />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+}
+</style>
